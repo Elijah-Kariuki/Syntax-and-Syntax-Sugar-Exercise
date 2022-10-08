95c30cbe9f32480bf113ba50e54f86fb33bd4194
Elijah Kariuki <ElijahKariuki1981@outlook.com>
2022-10-08 00:38:21 -0400

    initial commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
+        public static string MyAnswer()
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to nine";
+            return response;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            Console.WriteLine($"My son is {answer} years old and understands that there are {answer} tires on dad's car");
+            Console.WriteLine(MyAnswer());
+
         }
     }
 }
